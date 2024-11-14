098755235ffdf6f3d0383e45302aff38e6a24c5a
hasnat79 <hasnatabdullah79@gmail.com>
2024-11-14 08:02:17 -0600

    data updated

---
+++ data/__init__.py
@@ -0,0 +1,14 @@
+real_coco_image_path = "VCT_2/data/dummy/sd3_image/0_real"
+
+sd3_image_path = "VCT_2/data/dummy/sd3_image/1_fake"
+
+
+dalle_image_path = "VCT_2/data/saved_images/dalle_image"
+
+mid_journey_image_path ="VCT_2/data/saved_images/midjourney_image"
+
+sd21_image_path = "VCT_2/data/saved_images/sd21_image"
+
+sdxl_image_path = "VCT_2/data/saved_images/sd21_image"
+
+
